3dd3373ecf3110438a6625f9d7df52d026330be5
Yashrt263 <86078569+Yashrt263@users.noreply.github.com>
2022-09-10 15:43:17 +0530

    Time: 30 ms (29.62%), Space: 12.6 MB (46.87%) - LeetHub

---
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> dp(n + 1, vector<int> (2 * k + 1, 0));
+        for(int stock = n - 1; stock >= 0; stock--){
+            for(int bs = 0; bs < 2 * k; bs++){
+                if(bs % 2 == 0){
+                    dp[stock][bs] = max(dp[stock + 1][bs], -prices[stock] + dp[stock + 1][bs + 1]);
+                }
+                else{
+                    dp[stock][bs] = max(dp[stock + 1][bs], prices[stock] + dp[stock + 1][bs + 1]);
+                }
+            }
+        }
+        return dp[0][0];
+    }
+};
