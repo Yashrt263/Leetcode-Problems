b9f1b8f9656d76b4a4a2d4b1fcc51ce2417b401d
Yashrt263 <86078569+Yashrt263@users.noreply.github.com>
2022-09-23 02:01:08 +0530

    Time: 818 ms (22.14%), Space: 109.1 MB (32.69%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -8,7 +8,7 @@ public:
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= m; j++){
                 if(a[i - 1] == b[j - 1]){
-                    dp[i][j] = max(dp[i][j], 1 + dp[i - 1][j - 1]);
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
                     ans = max(ans, dp[i][j]);
                 }
             }
