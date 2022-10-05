972682eab52f3061821bcad3c61f54843e6d4134
Yashrt263 <86078569+Yashrt263@users.noreply.github.com>
2022-10-06 02:09:22 +0530

    Time: 241 ms (5.15%), Space: 8.4 MB (58.05%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
-    int numRollsToTarget(int d, int f, int target) {
-  vector<int> dp(target + 1);
-  dp[0] = 1;
-  for (int i = 1; i <= d; ++i) {
-    vector<int> dp1(target + 1);
-    for (int j = 1; j <= f; ++j)
-      for (auto k = j; k <= target; ++k)
-        dp1[k] = (dp1[k] + dp[k - j]) % 1000000007;
-    swap(dp, dp1);
-  }
-  return dp[target];
-}
-    // const int mod = 1e9 + 7;
-    // int numRollsToTarget(int n, int k, int target) {
-    //     vector<vector<int>> dp(n+1, vector<int> (target+1));
-    //     for(int i = n; i <= target; i++){
-    //         for(int )
+    const int mod = 1e9 + 7;
+    // int ans = 0;
+    // void recur(int n, int k, int target){
+    //     if(target < 0 || n < 0)
+    //         return;
+    //     if(n == 0 && target == 0)
+    //         ans += 1;
+    //     for(int i = 1; i <= k; i++){
+    //         recur(n - 1, k, target - i);
     //     }
-    //     return 0;
     // }
+    int numRollsToTarget(int n, int k, int target) {
+        vector<vector<int>> dp(n+1, vector<int> (target + 1));
+        for(int i = 1; i <= min(k, target); i++){
+            dp[1][i] = 1;
+        }
+        for(int dice = 1; dice <= n; dice++){
+            for(int face = 1; face <= k; face++){
+                for(int tar = 1; tar <= target; tar++){
+                    if(dice - 1 >= 0 && tar - face >= 0)
+                        dp[dice][tar] = (dp[dice][tar] + dp[dice - 1][tar - face]) % mod;
+                }
+            }
+        }
+        return dp[n][target];
+    }
 };
