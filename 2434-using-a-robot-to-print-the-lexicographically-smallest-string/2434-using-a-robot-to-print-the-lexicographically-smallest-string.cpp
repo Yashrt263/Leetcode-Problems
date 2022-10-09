0762f4e2d934311bcce49c1b5344bfa013293185
Yashrt263 <86078569+Yashrt263@users.noreply.github.com>
2022-10-10 04:10:13 +0530

    Time: 386 ms (11.11%), Space: 29.8 MB (100.00%) - LeetHub

--- 2434-using-a-robot-to-print-the-lexicographically-smallest-string/2434-using-a-robot-to-print-the-lexicographically-smallest-string.cpp
+++ 2434-using-a-robot-to-print-the-lexicographically-smallest-string/2434-using-a-robot-to-print-the-lexicographically-smallest-string.cpp
@@ -1,79 +1,31 @@
 class Solution {
 public:
+    char low(vector<int> & freq){ 
+        for(int i=0;i<26;i++){
+            if(freq[i]!=0)return 'a'+i;
+        } 
+        return 'z';   
+    }
+    
     string robotWithString(string s) {
-        string ans = "";
-        int  n = s.length();
-        vector<vector<int>> index(26);
-        for(int i = 0; i < n; i++){
-            index[s[i] - 'a'].push_back(i);
+        stack<char> t;
+        string ans="";  
+        vector<int> freq(26,0);
+        for(char c:s){
+            freq[c-'a']++;
         }
-        int prev = -1;
-        for(int i = 0; i < 26; i++){
-            if(!index[i].empty()){
-                if(prev == -1){
-                    prev = index[i][(int)index[i].size()-1];
-                }
-                else{
-                    index[i].erase(index[i].begin(), upper_bound(index[i].begin(), index[i].end(), prev));
-                    if(!index[i].empty())
-                        prev = index[i][(int)index[i].size()-1];
-                }
-            }
+        for(char c : s){
+            t.push(c);
+            freq[c-'a']--; 
+            while(t.size()>0 && t.top()<=low(freq)){
+                char x = t.top(); 
+                ans+= x;
+                t.pop();  
+            }    
         }
-        for(int i = 0; i < 26; i++){
-            if(!index[i].empty()){
-                for(int j : index[i]){
-                    cout << j << " ";
-                }
-                cout << endl;
-            }
-        }
-        //string ans = "";
-        stack<char> st;
-        int start = 0, end = 0;
-        for(int i = 0; i < 26; i++){
-            if(!index[i].empty()){
-                while(!st.empty() && st.top() - 'a' <= i){
-                    ans += st.top();
-                    st.pop();
-                }
-                while(start < index[i][0]){
-                    st.push(s[start]);
-                    start++;
-                }
-                start = index[i][0];
-                end = index[i][(int)index[i].size()-1];
-                while(start <= end){
-                    if(s[start] - 'a' == i){
-                        ans += s[start];
-                    }
-                    else{
-                        st.push(s[start]);
-                    }
-                    start++;
-                }
-            }
-        }
-        // if(!st.empty() && st.top() == s[start]){
-        //         ans += st.top();
-        //         st.pop();
-        //         ans += s[start];
-        //         start++;
-        //     }
-        //     else 
-        while(start < n){
-            if(!st.empty() && st.top() < s[start]){
-                ans += st.top();
-                st.pop();
-            }
-            else{
-                ans += s[start];
-                start++;
-            }
-        }
-        while(!st.empty()){
-            ans += st.top();
-            st.pop();
+        while(t.size()>0){
+             ans+= t.top();
+             t.pop();   
         }
         return ans;
     }
