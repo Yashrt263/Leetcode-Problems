694d734f03d5e92dfb279f36d50d53a237951af8
Yashrt263 <86078569+Yashrt263@users.noreply.github.com>
2022-10-09 07:00:04 +0530

    Time: 233 ms (95.91%), Space: 16.4 MB (48.63%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.cpp
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -2,20 +2,34 @@ class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
         sort(nums.begin(), nums.end());
-        int n = nums.size(), ans = nums[0] + nums[1] + nums[2];
-        for (int i = 0; i < n-2; ++i) {
-            int l = i + 1, r = n - 1;
-            while (l < r) {
-                int sum3 = nums[i] + nums[l] + nums[r];
-                if (abs(ans - target) > abs(sum3 - target)) // Update better ans
-                    ans = sum3;
-                if (sum3 == target) break;
-                if (sum3 > target)
-                    --r; // Sum3 is greater than the target, to minimize the difference, we have to decrease our sum3
+        int n = nums.size();
+        int k = 1e5;
+        int sign = 0;
+        for(int i = 0; i < n - 2; i++){
+            int new_target = target - nums[i];
+            int left = i + 1;
+            int right = n - 1;
+            while(left < right){
+                int remain = nums[left] + nums[right];
+                if(remain == new_target)
+                    return target;
+                int tmp = abs(new_target - remain);
+                if(tmp < k){
+                    k = tmp;
+                    if(remain > new_target){
+                        sign = 1;
+                    }
+                    else{
+                        sign = -1;
+                    }
+                }
+                if(remain > new_target){
+                    right--;
+                }
                 else
-                    ++l; // Sum3 is lesser than the target, to minimize the difference, we have to increase our sum3
+                    left++;
             }
         }
-        return ans;
+        return target + k*sign;
     }
 };
