af3afbae9c6bfd41d6f06cdce8a955924d12d28d
Yashrt263 <86078569+Yashrt263@users.noreply.github.com>
2022-11-22 05:15:39 +0530

    Time: 159 ms (85.03%), Space: 38.4 MB (20.62%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
@@ -1,43 +1,56 @@
 class Solution {
 public:
     int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
-        int rows = int(maze.size()), cols = int(maze[0].size());
-        vector<pair<int, int>> dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        
-        // Mark the entrance as visited since its not a exit.
-        int startRow = entrance[0], startCol = entrance[1];
-        maze[startRow][startCol] = '+';
-        
-        // Start BFS from the entrance, and use a queue `queue` to store all 
-        // the cells to be visited.
-        queue<array<int, 3>> queue;
-        queue.push({startRow, startCol, 0});
-        
-        while (!queue.empty()) {
-            auto [currRow, currCol, currDistance] = queue.front();
-            queue.pop();
-            
-            // For the current cell, check its four neighbor cells.
-            for (auto dir : dirs) {
-                int nextRow = currRow + dir.first, nextCol = currCol + dir.second;
-
-                // If there exists an unvisited empty neighbor:
-                if (0 <= nextRow && nextRow < rows && 0 <= nextCol && nextCol < cols \
-                   && maze[nextRow][nextCol] == '.') {
-                    
-                    // If this empty cell is an exit, return distance + 1.
-                    if (nextRow == 0 || nextRow == rows - 1 || nextCol == 0 || nextCol == cols - 1)
-                        return currDistance + 1;
-                    
-                    // Otherwise, add this cell to 'queue' and mark it as visited.
-                    maze[nextRow][nextCol] = '+';
-                    queue.push({nextRow, nextCol, currDistance + 1});
-                    
+        int n = maze.size();
+        int m = maze[0].size();
+        vector<vector<int>> visited(n, vector<int>(m, 0));
+        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(maze[i][j] == '+')
+                    visited[i][j] = 1;
+            }
+        }
+        visited[entrance[0]][entrance[1]] = 1;
+        dist[entrance[0]][entrance[1]] = 0;
+        auto inside = [&](int row, int col){
+            return row >= 0 && row < n && col >= 0 && col < m;
+        };
+        vector<pair<int, int>> dir = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+        queue<pair<int, int>> q;
+        q.push({entrance[0], entrance[1]});
+        int ans = INT_MAX;
+        while(!q.empty()){
+            pair<int, int> p = q.front();
+            q.pop();
+            for(pair<int, int> d : dir){
+                int new_x = d.first + p.first;
+                int new_y = d.second + p.second;
+                if(inside(new_x, new_y) && !visited[new_x][new_y]){
+                    q.push({new_x, new_y});
+                    dist[new_x][new_y] = min(dist[new_x][new_y], dist[p.first][p.second] + 1);
+                    visited[new_x][new_y] = 1;
                 }
             }
         }
-        
-        // If we finish iterating without finding an exit, return -1.
-        return -1;
+        // for(int i = 0; i < n; i++){
+        //     for(int j = 0; j < m; j++){
+        //         cout << dist[i][j] << " ";
+        //     }
+        //     cout << endl;
+        // }
+        for(int i = 0; i < n; i++){
+            if(dist[i][0] != 0)
+                ans = min(ans, dist[i][0]);
+            if(dist[i][m - 1] != 0)
+                ans = min(ans, dist[i][m - 1]);
+        }
+        for(int i = 0; i < m; i++){
+            if(dist[0][i] != 0)
+                ans = min(ans, dist[0][i]);
+            if(dist[n - 1][i] != 0)
+                ans = min(ans, dist[n - 1][i]);
+        }
+        return ans == INT_MAX ? -1 : ans;
     }
 };
