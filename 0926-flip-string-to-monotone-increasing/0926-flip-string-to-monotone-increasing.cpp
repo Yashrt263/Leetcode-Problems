5b17bd703c035d67edf46baba0715467e5c1b9f2
Yashrt263 <86078569+Yashrt263@users.noreply.github.com>
2023-01-18 00:42:16 +0530

    Time: 47 ms (51.39%), Space: 20 MB (25.06%) - LeetHub

---
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minFlipsMonoIncr(string s) {
+        int n = s.length();
+        vector<int> c0(n);
+        vector<int> c1(n);
+        c0[0] = s[0] == '0' ? 0 : 1;
+        c1[n - 1] = s[n - 1] == '0' ? 1 : 0;
+        for(int i = 1; i < n; i++){
+            if(s[i] == '0'){
+                c0[i] += c0[i - 1];
+            }
+            else{
+                c0[i] += c0[i - 1] + 1;
+            }
+        }
+        for(int i = n - 2; i >= 0; i--){
+            if(s[i] == '1'){
+                c1[i] += c1[i + 1];
+            }
+            else{
+                c1[i] += c1[i + 1] + 1;
+            }
+        }
+        int ans = min(c0[n - 1], c1[0]);
+        for(int i = 1; i < n; i++){
+            ans = min(ans, c1[i] + c0[i - 1]);
+        }
+        // for(int i : c0) cout << i << " ";
+        // cout << endl;
+        // for(int i : c1) cout << i << " ";
+        return ans;
+    }
+};
