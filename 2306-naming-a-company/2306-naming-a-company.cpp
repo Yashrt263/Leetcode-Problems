a0663fc3ea6951525c29ba0830f376c397f8440a
Yashrt263 <86078569+Yashrt263@users.noreply.github.com>
2023-02-10 03:24:56 +0530

    Time: 525 ms (86.99%), Space: 83.4 MB (56.91%) - LeetHub

--- 2306-naming-a-company/2306-naming-a-company.cpp
+++ 2306-naming-a-company/2306-naming-a-company.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
     long long distinctNames(vector<string>& ideas) {
-        // Group idea by their initials.
-        unordered_set<string> initialGroup[26];
-        for (auto& idea : ideas)
-            initialGroup[idea[0] - 'a'].insert(idea.substr(1));
-        
-        // Calculate number of valid names from every pair of groups.
-        long long answer = 0;
-        for (int i = 0; i < 25; ++i) {
-            for (int j = i + 1; j < 26; ++j) {
-                // Get the number of mutual suffixes.
-                int numOfMutual = 0;
-                for (auto& ideaA : initialGroup[i]) {
-                    if (initialGroup[j].count(ideaA)) {
-                        numOfMutual++;
-                    }
+        long long ans = 0;
+        unordered_set<string> s[26];
+        for(string i : ideas){
+            s[i[0] - 'a'].insert(i.substr(1));
+        }
+        for(int i = 0; i < 25; i++){
+            for(int j = i + 1; j < 26; j++){
+                int mut = 0;
+                for(auto ideaA : s[i]){
+                    if(s[j].count(ideaA)) mut++;
                 }
-
-                // Valid names are only from distinct suffixes in both groups.
-                // Since we can swap a with b and swap b with a to create two valid names, multiple answer by 2.
-                answer += 2LL * (initialGroup[i].size() - numOfMutual) * (initialGroup[j].size() - numOfMutual);
+                ans += 2 * (s[i].size() - mut) * (s[j].size() - mut);
             }
         }
-        return answer;
+        return ans;
     }
 };
