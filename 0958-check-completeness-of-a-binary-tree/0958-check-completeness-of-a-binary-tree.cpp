e4914f0b62d1ef6b86af3d99453df114b9d0c0cc
Yashrt263 <86078569+Yashrt263@users.noreply.github.com>
2023-03-15 12:27:46 +0530

    Time: 10 ms (19.69%), Space: 10.4 MB (59.30%) - LeetHub

--- 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
@@ -3,44 +3,16 @@ public:
     bool isCompleteTree(TreeNode* root) {
         queue<TreeNode*> q;
         q.push(root);
-        int height = 0;
+        int f = 0;
         while(!q.empty()){
-            int n = q.size();
-            while(n--){
-                TreeNode* t = q.front();
-                q.pop();
-                if(t->left != NULL) q.push(t->left);
-                if(t->right != NULL) q.push(t->right);
-            }
-            height++;
-        }
-        //cout << height << endl;
-        q.push(root);
-        while(!q.empty()){
-            int n = q.size();
-            if(height > 2){
-                while(n--){
-                    TreeNode* t = q.front();
-                    q.pop();
-                    if(t->left == NULL || t->right == NULL) return false;
-                    q.push(t->left);
-                    q.push(t->right);
-                }
-            }
+            TreeNode* t = q.front();
+            q.pop();
+            if(t == NULL) f = 1;
             else{
-                int f = 0;
-                //cout << n << endl;
-                while(n--){
-                    TreeNode* t = q.front();
-                    q.pop();
-                    if((t->left == NULL && t->right != NULL) || ((t->left != NULL || t->right != NULL) && f == 1)){
-                        //cout << n << endl;
-                        return false;
-                    }
-                    if((t->left != NULL && t->right == NULL) || (t->left == NULL && t->right == NULL)) f = 1;
-                }
+                if(f) return false;
+                q.push(t->left);
+                q.push(t->right);
             }
-            height--;
         }
         return true;
     }
