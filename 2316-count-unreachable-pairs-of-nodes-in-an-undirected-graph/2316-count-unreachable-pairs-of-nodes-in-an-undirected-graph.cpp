8b60e4ced7c84451b4129cf34aea1299f3dab84b
Yashrt263 <86078569+Yashrt263@users.noreply.github.com>
2023-03-25 10:26:26 +0530

    Time: 750 ms (30.95%), Space: 304.4 MB (5.04%) - LeetHub

---
+++ 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    long long countPairs(int n, vector<vector<int>>& edges) {
+        long long ans = 0;
+        vector<vector<int>> adj(n);
+        for(vector<int> v : edges){
+            adj[v[0]].push_back(v[1]);
+            adj[v[1]].push_back(v[0]);
+        }
+        vector<int> visited(n);
+        vector<int> tmp;
+        long long total = 0;
+        for(int i = 0; i < n; i++){
+            if(!visited[i]){
+                queue<int> q;
+                q.push(i);
+                visited[i] = 1;
+                int cnt = 0;
+                while(!q.empty()){
+                    int node = q.front();
+                    q.pop();
+                    cnt++;
+                    for(int nxt : adj[node]){
+                        if(!visited[nxt]){
+                            q.push(nxt);
+                            visited[nxt] = 1;
+                        }
+                    }
+                }
+                tmp.push_back(cnt);
+                total += cnt;
+            }
+        }
+        for(int i : tmp){
+            ans += i * (total - i);
+            total -= i;
+        }
+        return ans;
+    }
+};
